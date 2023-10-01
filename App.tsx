b1dcd15eed9a646a7140e392e134f18b8c43f22d
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2023-10-01 22:24:05 +0000

    add folders and icons

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import { Text, View } from 'react-native';
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
   return (
-    <View className='flex justify-center items-center w-screen h-screen'>
+    <View className='flex justify-center items-center w-screen h-screen bg-black'>
       <Text className="text-3xl text-red-300">Open up App.tsx to start working on your app!</Text>
     </View>
   );
